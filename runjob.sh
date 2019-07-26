d9dd90c50806c64ed3ea925070706c8d741c6ba9
Casey Faist <caseylfaist@gmail.com>
2019-07-26 16:26:00 -0400

    correct pathing so versions file is added to s3

--- runjob.sh
+++ runjob.sh
@@ -4,6 +4,6 @@ xargs -P 1 -n 1 pyenv install < python-versions.txt
 
 bash archive.sh
 
-cp python-versions.txt /heroku-18
+cp python-versions.txt heroku-18
 
 aws s3 sync heroku-18 s3://lang-python-staging/heroku-18 --delete
